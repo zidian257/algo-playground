1690f1f8549e584c546b4e2a9e8d37cac364ea56
zidian257@15in-mbp <zidian257@gmail.com>
2019-11-28 20:27:51 +0800

    mindepth binary tree

--- src/binaryTreeMinDepth/index.js
+++ src/binaryTreeMinDepth/index.js
@@ -3,8 +3,8 @@
 //         1
 //      /   \
 //     2     4
-//          / \
-//         8  16
+//   /      / \
+//  3       8  16
 //
 // 这棵树有 3 个叶子节点，分别是 2，8，16。最近的叶子节点是 2，根节点到 2 共有两个节点，因此最小深度是 2。
 //
@@ -17,6 +17,7 @@
 //       4
 //
 // 这棵树唯一的叶子节点是 4，根节点到它共有 3 个节点，因此最小深度是 3。
+
 class TreeNode {
   constructor(val, left, right) {
     this.left = left;
@@ -24,7 +25,7 @@ class TreeNode {
     this.val = val;
   }
 }
-const t = new TreeNode(3, new TreeNode(4));
+const t = new TreeNode(3, new TreeNode(4), new TreeNode(5));
 
 const minDepth = tree => {
   if (!tree) {
@@ -47,6 +48,31 @@ const minDepth = tree => {
   }
 };
 
-const gg = minDepth(t);
+const minDepthByQueue = tree => {
+  if (!tree) {
+    return 0;
+  }
+  const queue = [];
 
+  queue.unshift(tree);
+  let count = 1;
+
+  let i = 0;
+  while (queue.length > 0) {
+    count += 1;
+
+    for (i = 0; i < queue.length; ++i) {
+      const node = queue.pop();
+
+      if (!node.left || !node.right) {
+        return count;
+      }
+      queue.unshift(node.left, node.right);
+    }
+  }
+
+  return -1;
+};
+
+const gg = minDepthByQueue(t);
 console.log('gg:', gg);
