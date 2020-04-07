53a0c0c5ed400d8e8ba35ff289c01875152d88da
binghua chen <nreal-bhchen@NREAL-BHCHEN-MBP15.local>
2020-04-07 19:26:04 +0800

    ..

--- src/heap/index.js
+++ src/heap/index.js
@@ -1,6 +1,15 @@
 // 实现一个 javascript 堆
 
+class CBTree {
+  // complete binary tree
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
 // todo
 const heap = () => {
-
-}
+  // const
+};
