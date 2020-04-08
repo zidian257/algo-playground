9ccb8c3343a94d3acdccf1bbc38a1b47a779f0ac
zidian257@15in-mbp <zidian257@gmail.com>
2020-04-09 02:19:41 +0800

    ..

--- src/heap/index.js
+++ src/heap/index.js
@@ -4,90 +4,76 @@
 //    / \
 //   1   2
 
-class CBTree {
-  // complete binary tree
-  constructor(val, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
-}
-
 class MinHeap {
-  // heap
   constructor(arr) {
-    if (!Array.isArray(arr) || arr.length === 0) {
-      throw 'N/A';
+    this.heap = [null];
+    this.size = 0;
+    if (Array.isArray(arr) && arr.length !== 0) {
+      this.build(arr);
     }
-    this.heapArr = [null];
-
-    this.build(arr);
-    // for (let i of arr) {
-    //   this.add(i);
-    // }
   }
-
-  add = elem => {
-    this.heapArr.push(elem);
-    let targetIndex = this.heapArr.length - 1;
-    let parentIndex;
-
-    while (this._getParentIndex(targetIndex) > 0 && targetIndex > 1) {
-      parentIndex = this._getParentIndex(targetIndex);
-
-      if (this.heapArr[targetIndex] < this.heapArr[parentIndex]) {
-        this._swap(targetIndex, parentIndex);
-      } else {
-        break;
-      }
-      targetIndex = parentIndex;
-    }
-  };
 
   percolatingDown = pos => {
+    const target = this.heap[pos];
+    let child;
 
+    for (; pos * 2 <= this.size; pos = child) {
+      child = pos * 2;
+      if (child !== this.size && this.heap[child + 1] < this.heap[child])
+        child++;
+
+      if (target > this.heap[child]) this.heap[pos] = this.heap[child];
+      else break;
+    }
+    this.heap[pos] = target;
   };
 
   percolatingUp = pos => {
-    const target = this.heapArr[pos];
+    const target = this.heap[pos];
     for (
       ;
-      pos > 1 && target < this.heapArr[Math.floor(pos / 2)];
+      pos > 1 && target < this.heap[Math.floor(pos / 2)];
       pos = Math.floor(pos / 2)
     )
-      this.heapArr[pos] = this.heapArr[Math.floor(pos / 2)];
+      this.heap[pos] = this.heap[Math.floor(pos / 2)];
 
-    this.heapArr[pos] = target;
+    this.heap[pos] = target;
   };
 
   insert = elem => {
-    this.heapArr.push(elem);
-    this.percolatingUp(this.heapArr.length - 1);
+    this.heap.push(elem);
+    this.percolatingUp(this.heap.length - 1);
   };
 
   build = arr => {
     for (let i of arr) {
       this.insert(i);
     }
+    this.size = arr.length;
   };
 
-  peak = () => this.heapArr.slice(1);
+  peak = () => this.heap.slice(1);
 
-  _getParentIndex = index => Math.floor(index / 2);
+  heapSort = arr => {
+    if (arr.length !== 0) {
+      this.build(arr);
+    }
 
+    for (let i = this.size; i > 0; i--) {
+      this._swap(i, 1);
+      this.size--;
+      this.percolatingDown(1);
+    }
+    return this.peak();
+
+  };
   _swap = (a, b) => {
-    let tmp = this.heapArr[a];
-    this.heapArr[a] = this.heapArr[b];
-    this.heapArr[b] = tmp;
-  };
-
-  size = () => this.heapArr.length - 1;
-
-  delMin = () => {
-    // todo
+    const tmp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = tmp;
   };
 }
 
-const gg = new MinHeap([7, 4, 3, 2, 53, 5, 6, 1]);
-
-console.log('gg.peak():', gg.peak());
+const gg = new MinHeap();
+const mm = gg.heapSort([7, 10, 6, 23, 1, 22, 8, 433, 12]);
+console.log('mm:', mm);
