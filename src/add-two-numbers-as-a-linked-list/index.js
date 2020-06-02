2ac4b6fd93fcd0a970687b707b1481309b5e79a7
zidian257@15in-mbp <zidian257@gmail.com>
2020-06-02 21:53:07 +0800

    ..

--- src/add-two-numbers-as-a-linked-list/index.js
+++ src/add-two-numbers-as-a-linked-list/index.js
@@ -1,4 +1,6 @@
-// You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+// You are given two linked-lists representing two non-negative integers.
+// The digits are stored in reverse order and each of their nodes contain a single digit.
+// Add the two numbers and return it as a linked list.
 //
 //   Example:
 // Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
@@ -17,25 +19,43 @@ l2.next.next = Node(4);
 
 const addTwoNumber = (node1, node2) => {
   let carry = 0;
-  let res = 0;
   let cur = 0;
+  const ret = Node(null);
+  let pointer = ret;
 
   let l1 = node1;
   let l2 = node2;
 
   while (l1 || l2) {
-    cur = l1.val + l2.val + carry;
+    let l1val = 0;
+    let l2val = 0;
+    if (l1) {
+      l1val = l1.val;
+    }
+
+    if (l2) {
+      l2val = l2.val;
+    }
+
+    cur = l1val + l2val + carry;
     carry = 0;
     if (cur >= 10) {
       cur = cur - 10;
       carry = 1;
     }
 
-    res = res * 10 + cur
+    pointer.next = Node(cur);
 
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+    pointer = pointer.next;
   }
 
-
+  return ret.next;
 };
 
 module.exports = { addTwoNumber, Node, l1, l2 };
--- src/add-two-numbers-as-a-linked-list/index.test.js
+++ src/add-two-numbers-as-a-linked-list/index.test.js
@@ -1,11 +1,37 @@
-const { addTwoNumber, Node, l1, l2 } = require('./index.js')
+// You are given two linked-lists representing two non-negative integers.
+// The digits are stored in reverse order and each of their nodes contain a single digit.
+// Add the two numbers and return it as a linked list.
+//
+//   Example:
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
 
+// 4 -> 5
+// 9
+
+const { addTwoNumber, Node, l1, l2 } = require('./index.js');
 
 test('Node constructor works', () => {
-  expect(Node(3).val).toBe(3)
-})
+  expect(Node(3).val).toBe(3);
+});
 
+const l3 = Node(4);
+l3.next = Node(5);
+const l4 = Node(9);
 
 test('add works', () => {
-  expect(addTwoNumber(l1, l2)).toBe(807)
-})
+  const ret = addTwoNumber(l1, l2);
+  const ret2 = addTwoNumber(l3, l4);
+  expect(ret.val).toBe(7);
+  expect(ret.next.val).toBe(0);
+  expect(ret.next.next.val).toBe(8);
+  expect(ret2.val).toBe(3);
+  expect(ret2.next.val).toBe(6);
+});
+
+
+test('linked list length', () => {
+  const ret = addTwoNumber(l1, l2);
+  expect(ret.next.next.next).toBe(null);
+});
