a502892120598313a213b0e47ee3cccf5aae1736
bhchen <bhchen@nreal.ai>
2020-10-10 11:05:11 +0800

    qsort

--- src/sorting-algorithms/quick_sort.js
+++ src/sorting-algorithms/quick_sort.js
@@ -5,14 +5,38 @@ const swap = (arr, p, q) => {
 };
 
 const partition = (arr, p, q) => {
-  const pivot = arr[q]; // selecting last element as pivot
+  if (q === p + 1) {
+    // 如果只有 2 位
+    if (arr[p] > arr[q]) {
+      swap(arr, p, q);
+    }
+    return p;
+  }
 
-  // let storeIndex = q;
-  // for (let i = p; i < q; i++) {
-  //   if (arr[i] > pivot) {
-  //
-  //   }
-  // }
+  // 快速排序就是每次在递归区间内，将基准数字归位的过程
+  const pivotIndex = Math.floor((p + q) / 2);
+  const pivot = arr[pivotIndex]; // 选择 (p+q)/2 为基准
+
+  swap(arr, pivotIndex, q); // 把基准放到最后一位
+
+  let i = p,
+    j = q - 1;
+
+  while (i < j) {
+    // 终止条件是 i j 相等
+
+    // 先从左边向右找到比 pivot 大的
+    while (arr[i] <= pivot && i < j) i++;
+    // 从右边向左找到比 pivot 小的
+    while (arr[j] >= pivot && i < j) j--;
+    // 交换
+    swap(arr, i, j);
+  }
+
+  // 基准归位
+  swap(arr, i, q);
+
+  return i;
 };
 
 const quick_sort = (arr, left = 0, right = arr.length - 1) => {
