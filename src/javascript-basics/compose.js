0073a545708754b0df8497fb788f2ea0f9b352da
Binghua Chen <bhchen@nreal.ai>
2020-12-23 19:22:33 +0800

    simple-composer

--- src/javascript-basics/compose.js
+++ src/javascript-basics/compose.js
@@ -1,8 +1,24 @@
 // non async compose
 
-
 // var greet = compose(hello, toUpperCase);
 // greet('kevin');
 
+const compose = (...fns) => {
+  return function(...args) {
+    return fns.reduce((accu, curr, index) => {
+      if (index === 0) {
+        return curr(...args);
+      } else {
+        return curr(accu);
+      }
+    }, 0);
+  };
+};
 
-// const compose =
+const hello = str => console.log(str + ' world!!!');
+
+const toUpperCase = str => str.toUpperCase();
+
+const greet = compose(toUpperCase, hello);
+
+greet('bh');
