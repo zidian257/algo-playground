2dfe4190d1c75656174659235f71cad377b50167
Binghua Chen <bhchen@nreal.ai>
2020-12-24 18:37:08 +0800

    wip

--- src/javascript-basics/compose.js
+++ src/javascript-basics/compose.js
@@ -21,7 +21,7 @@ const toUpperCase = str => str.toUpperCase();
 
 const greet = pipe(toUpperCase, hello);
 
-greet('bh');
+// greet('bh');
 
 // 从右边开始计算
 const compose = (...fns) => {
@@ -39,4 +39,71 @@ const compose = (...fns) => {
 
 const greet2 = compose(hello, toUpperCase);
 
-greet2('bh');
+// greet2('bh');
+
+// async compose
+//
+
+// Un.request(config).then();
+//
+// Un.request = composeP(mid1, mid2, request);
+
+const mid1 = (config, next) => {
+  // do something with config
+  return next(config).then(res => {
+    // do something with return value
+    res.a = 3;
+    return res;
+  });
+};
+
+const mid2 = async (config, next) => {
+  // do something with config
+
+  const res = await next(config);
+
+  res.b = 4;
+  return res;
+};
+
+//
+// const request = composeP([mid1, mid2, request])
+// request(these).then(that)
+
+const composeP = (middleware, request) => {
+  return config => {
+
+
+
+
+
+    return middleware.reduce((accu, curr) => {
+      return curr(config, accu);
+    }, request);
+  };
+};
+
+const originRq = config => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        origin: 32
+      });
+    }, 1999);
+  });
+};
+
+const logger =  (config, next) => {
+  console.log('config:', config);
+  return next(config).then(r => {
+    console.log(r);
+
+    return r;
+  });
+};
+
+const request = composeP([mid1, mid2, logger], originRq);
+
+request({
+  cf: 32
+});
